313e312eaef912e07e874b6d38c9c67623b81d09
uzairqq <uzair.qq@outlook.com>
2017-07-17 15:58:59 +0500

    added services.addMvc() and app.useMvc()

--- TheWorld/TheWorld/Controllers/AppController.cs
+++ TheWorld/TheWorld/Controllers/AppController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace TheWorld.Controllers
 {
-    public class AppController :Controller 
+    [Route("api/[controller]")]
+    public class AppController : Controller
     {
         public IActionResult Index()
         {
--- TheWorld/TheWorld/Startup.cs
+++ TheWorld/TheWorld/Startup.cs
@@ -16,15 +16,16 @@ namespace TheWorld
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app)
         {
             //app.UseDefaultFiles(); // to serve the files and page in entring the localhost and not doing with doing like eg:- http://localhost:0000/index.html
 
             app.UseStaticFiles(); //this is the middleWare to serve static files like html,javascript and css
-          
+            app.UseMvc();
         }
     }
 }
