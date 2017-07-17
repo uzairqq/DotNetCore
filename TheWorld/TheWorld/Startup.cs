72216b2090871945b8585b61775d801cfdb43bd6
uzairqq <uzair.qq@outlook.com>
2017-07-17 14:38:32 +0500

    serving static files using app.useStaticFiles() and index in www root

--- TheWorld/TheWorld/Startup.cs
+++ TheWorld/TheWorld/Startup.cs
@@ -21,17 +21,7 @@ namespace TheWorld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("<html><h1>Hello World!</h1></html>");
-            });
+            app.UseStaticFiles();
         }
     }
 }
