0950b246861be43d83561948bc6a8b913635dcad
uzairqq <uzair.qq@outlook.com>
2017-07-20 15:52:25 +0500

    added validation on contactview model class for server side validation

--- TheWorld/TheWorld/ViewModels/ContactViewModel.cs
+++ TheWorld/TheWorld/ViewModels/ContactViewModel.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.Web.CodeGeneration.Utils.Messaging;
 
 namespace TheWorld.ViewModels
 {
     public class ContactViewModel
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Message { get; set; }
     }
 }
