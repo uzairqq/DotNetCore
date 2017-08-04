09080cf97fcfa3ad77d95bbc3aca335acf1e4646
uzairqq <uzair.qq@outlook.com>
2017-08-04 15:23:29 +0500

    added full bootstrap but not understanded and all bootstrap included

--- TheWorld/TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/TheWorld/Controllers/Web/AppController.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using TheWorld.Services;
--- TheWorld/TheWorld/wwwroot/js/site.js
+++ TheWorld/TheWorld/wwwroot/js/site.js
@@ -1,19 +1,21 @@
 // site.js
 (function () {
-  //var ele = $("#userName");
+
+  //var ele = $("#username");
   //ele.text("Shawn Wildermuth");
 
   //var main = $("#main");
   //main.on("mouseenter", function () {
-  //  main.css("background-color", "#888");
+  //  main.style = "background-color: #888;";
   //});
   //main.on("mouseleave", function () {
-  //  main.css("background-color", "");
+  //  main.style = "";
   //});
 
-  //$(".menu li a").on("click", function () {
-  //  alert($(this).text());
-  //  return false;
+  //var menuItems = $("ul.menu li a");
+  //menuItems.on("click", function () {
+  //  var me = $(this);
+  //  alert(me.text());
   //});
 
   var $sidebarAndWrapper = $("#sidebar,#wrapper");
@@ -22,12 +24,11 @@
   $("#sidebarToggle").on("click", function () {
     $sidebarAndWrapper.toggleClass("hide-sidebar");
     if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-        $icon.removeClass("fa-angle-left");
-        $icon.addClass("fa-angle-right");
+      $icon.removeClass("fa-angle-left");
+      $icon.addClass("fa-angle-right");
     } else {
-        $icon.addClass("fa-angle-left");
-        $icon.removeClass("fa-angle-right");
+      $icon.removeClass("fa-angle-right");
+      $icon.addClass("fa-angle-left");
     }
   });
-
 })();
